c5cad1b2a3ae31abfb9c04bff5fd6c4e18d814f2
Jurini <curvello_@hotmail.com>
2020-08-15 14:53:31 -0300

    Desafio -- Aprovação de crédito

---
+++ src/AprovaCredito.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class AprovaCredito {
+    public static void main(String[] args) {
+        DecimalFormat form = new DecimalFormat("#00.00");
+
+        double renda,parcela,fatorAprovador = 40,fatorReal;
+
+        renda = Double.parseDouble(JOptionPane.showInputDialog
+                ("Informe sua renda"));
+        parcela = Double.parseDouble(JOptionPane.showInputDialog
+                ("Informe o valor da parcela do bem"));
+
+        fatorReal = (parcela * 100) / renda;
+
+        if(fatorReal <= fatorAprovador){
+            JOptionPane.showMessageDialog
+                    (null,"Parabéns, crédito aprovado");
+        }else {
+            JOptionPane.showMessageDialog
+                    (null,"Percentual da renda comprometido pela parcela "+form.format(fatorReal)+"%");
+            JOptionPane.showMessageDialog
+                    (null,"Infelizmente seu crédito não será aprovado");
+        }
+
+    }
+}
