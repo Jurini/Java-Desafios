8d430ed03eaf1fc478c3e5a03e6f4aeff03f7947
Jurini <curvello_@hotmail.com>
2020-08-17 10:58:03 -0300

    Desafio -- Mostrar os numeros ímpares do array.

---
+++ src/Ibrantan.java
@@ -0,0 +1,12 @@
+public class Ibrantan {
+    public static void main(String[] args) {
+        int numeros[] = {5, 8, 7, 9, 2, 5, 3};
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 1) {
+                System.out.println(numeros[i]);
+            }
+        }
+    }
+}
+
